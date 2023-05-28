527c3dc3c9f183e3da64cc6f267001a0e794554a
2-МД-8 Калашников Максим <99969404+maxim4ik228@users.noreply.github.com>
2023-05-28 22:44:55 +0300

    Game

---
+++ cmake-build-debug/CMakeFiles/untitled2.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/Максик/CLionProjects/untitled2/DrawText.cpp" "CMakeFiles/untitled2.dir/DrawText.cpp.obj" "gcc" "CMakeFiles/untitled2.dir/DrawText.cpp.obj.d"
+  "C:/Users/Максик/CLionProjects/untitled2/GetTetromino.cpp" "CMakeFiles/untitled2.dir/GetTetromino.cpp.obj" "gcc" "CMakeFiles/untitled2.dir/GetTetromino.cpp.obj.d"
+  "C:/Users/Максик/CLionProjects/untitled2/GetWallKickData.cpp" "CMakeFiles/untitled2.dir/GetWallKickData.cpp.obj" "gcc" "CMakeFiles/untitled2.dir/GetWallKickData.cpp.obj.d"
+  "C:/Users/Максик/CLionProjects/untitled2/Tetromino.cpp" "CMakeFiles/untitled2.dir/Tetromino.cpp.obj" "gcc" "CMakeFiles/untitled2.dir/Tetromino.cpp.obj.d"
+  "C:/Users/Максик/CLionProjects/untitled2/main.cpp" "CMakeFiles/untitled2.dir/main.cpp.obj" "gcc" "CMakeFiles/untitled2.dir/main.cpp.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/untitled2.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/untitled2.dir/DrawText.cpp.obj"
+  "CMakeFiles/untitled2.dir/DrawText.cpp.obj.d"
+  "CMakeFiles/untitled2.dir/GetTetromino.cpp.obj"
+  "CMakeFiles/untitled2.dir/GetTetromino.cpp.obj.d"
+  "CMakeFiles/untitled2.dir/GetWallKickData.cpp.obj"
+  "CMakeFiles/untitled2.dir/GetWallKickData.cpp.obj.d"
+  "CMakeFiles/untitled2.dir/Tetromino.cpp.obj"
+  "CMakeFiles/untitled2.dir/Tetromino.cpp.obj.d"
+  "CMakeFiles/untitled2.dir/main.cpp.obj"
+  "CMakeFiles/untitled2.dir/main.cpp.obj.d"
+  "libuntitled2.dll.a"
+  "untitled2.exe"
+  "untitled2.exe.manifest"
+  "untitled2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/untitled2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
